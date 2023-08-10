5a6aeee02b323870f7e2cb596e50e7b033fa7197
snow <matt01010000@gmail.com>
2023-08-09 23:30:17 -0400

    Switched to an API called WeatherAPI.com, because the API I was trying to use is broken

--- src/hooks/GetWeather.js
+++ src/hooks/GetWeather.js
@@ -9,10 +9,10 @@ export const GetWeather = () => {
   const [lat, setLat] = useState([]);
   const [lon, setLon] = useState([]);
 
-  const fetchWeatherData = async () => {
+  const fetchWeatherData = async (lat, lon) => {
     try {
       const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${lat},${lon}`
       );
       const data = await res.json();
       setWeather(data);
@@ -34,8 +34,54 @@ export const GetWeather = () => {
       let location = await Location.getCurrentPositionAsync({});
       setLat(location.coords.latitude);
       setLon(location.coords.longitude);
-      await fetchWeatherData();
+      await fetchWeatherData(
+        location.coords.latitude,
+        location.coords.longitude
+      );
     })();
-  }, [lat, lon]);
+  }, []);
+
   return [loading, error, weather];
 };
+
+// import React, { useState, useEffect } from "react";
+// import * as Location from "expo-location";
+// import { WEATHER_API_KEY } from "@env";
+
+// export const GetWeather = () => {
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [weather, setWeather] = useState([]);
+//   const [lat, setLat] = useState([]);
+//   const [lon, setLon] = useState([]);
+
+//   const fetchWeatherData = async () => {
+//     try {
+//       const res = await fetch(
+//         `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+//       );
+//       const data = await res.json();
+//       setWeather(data);
+//     } catch (err) {
+//       setError("could not fetch weather");
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     (async () => {
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+
+//       if (status !== "granted") {
+//         setError("permission to access location was denied");
+//         return;
+//       }
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLat(location.coords.latitude);
+//       setLon(location.coords.longitude);
+//       await fetchWeatherData();
+//     })();
+//   }, [lat, lon]);
+//   return [loading, error, weather];
+// };
--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,39 +1,40 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import { GetWeather } from "../hooks/GetWeather";
+
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "grey",
+  },
+  container2: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "cyan",
+  },
+});
 
 const CurrentWeather = () => {
+  const [loading, error, weather] = GetWeather();
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error: {error}</Text>;
+  }
+
   return (
     <React.Fragment>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "grey",
-        }}
-      >
-        <Text>Weather Condition is Rainy</Text>
+      <View style={styles.container1}>
+        <Text>Weather Description: {weather.current.condition.text}</Text>
       </View>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "cyan",
-        }}
-      >
-        <Text>Temperature: 68</Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "grey",
-        }}
-      >
-        <Text>High: 75</Text>
-        <Text>Low: 67</Text>
+      <View style={styles.container2}>
+        <Text>Temperature: {weather.current.temp_c} °C</Text>
       </View>
     </React.Fragment>
   );
